c14ada022945bc73b30f4c740effc5a74189a31d
Zheng Zhaoxiang <Zhaoxiang.zheng@bmw.partner.com>
2018-07-19 14:44:11 +0800

    commit the python demos which wrote in 20180718

---
+++ 20180718/print.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+print("100+200=",100+200)
+
+#get input value
+name = input("Please input your name or something:\n")
+print("I","am",name)
+
+
+print("2014 * 768 = ", 1024*768)
+
+
