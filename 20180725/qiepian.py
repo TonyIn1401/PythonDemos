5622efdf2f27031607791c795a94020ad1136694
TonyIn1401 <zhx1990410@yeah.net>
2018-07-30 15:14:37 +0800

    asd

---
+++ 20180725/qiepian.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+
+def qiepiandemo():
+    L = ['Michael', 'tony', 'Tracy', 'bob']
+    print(L[0])
+    for item in L:
+        print(item)
+
+def qiepiandemo2():
+    L = [1,2,3,4,5,6,7,8,9]
+    print(L[0:3])
+    print(L[:3])
+    print(L[-4:])
+
+qiepiandemo2()
+qiepiandemo()
+
